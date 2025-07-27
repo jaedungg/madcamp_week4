89cc88d3266dc6439217c8e0152ea8304ccba026
jaedungg <149063479+jaedungg@users.noreply.github.com>
2025-07-28 01:46:28 +0900

    feat: POST docs access log API

--- src/app/api/documents/[id]/favorite/route.ts
+++ src/app/api/documents/[id]/favorite/route.ts
@@ -1,4 +1,4 @@
-// app/api/documents/[id]/duplicate/route.ts
+// app/api/documents/[id]/favorite/route.ts
 
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
